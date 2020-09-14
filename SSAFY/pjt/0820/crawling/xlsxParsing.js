9017f3b95bf02dc3b77cec9f7c04d55b4c777e6c
송건호 <gun2841@naver.com>
2020-09-14 15:02:57 +0900

    delete

--- SSAFY/pjt/0820/crawling/xlsxParsing.js
+++
@@ -1,7 +0,0 @@
-const  xlsx = require("xlsx");
-const book = xlsx.readFile("./xlsx/data-movie.xlsx");
-
-const b = book.Sheets.영화목록;
-
-const records = xlsx.utils.sheet_to_json(b);
-console.log(records);
